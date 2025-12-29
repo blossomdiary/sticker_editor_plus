2071a6ddf527c04952622c1a8194b505365e7083
Jinny You <baram991103@gmail.com>
2025-12-29 14:08:03 +0900

    Mark Windows/Linux as supported
    This package doesn’t have any platform-specific dependencies. However, it was originally created as a plugin project, so Windows and Linux aren’t currently marked as supported.
    In the long run, it may be better to migrate it to a pure Dart package.

---
+++ lib/src/sticker_editor_windows.dart
@@ -0,0 +1,4 @@
+/// Windows implementation placeholder for the plugin registration.
+class StickerEditorWindows {
+  static void registerWith() {}
+}
--- lib/sticker_editor_plus.dart
+++ lib/sticker_editor_plus.dart
@@ -3,3 +3,5 @@ export 'src/stickereditor.dart'
 export 'src/model/picture_model.dart' show PictureModel;
 export 'src/model/sticker_placement.dart' show StickerPlacement;
 export 'src/model/text_model.dart' show TextModel;
+export 'src/sticker_editor_linux.dart' show StickerEditorLinux;
+export 'src/sticker_editor_windows.dart' show StickerEditorWindows;
